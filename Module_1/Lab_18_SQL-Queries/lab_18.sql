a909a99fd38d2d0178e893803ff035e63cd48136
katerinakovaleva201 <kovkatja@gmail.com>
2023-02-12 21:11:42 +0100

    New labs

--- Module_1/Lab_18_SQL-Queries/lab_18.sql
+++ Module_1/Lab_18_SQL-Queries/lab_18.sql
@@ -1,9 +1,73 @@
-create database applestore;
-use applestore;
-CREATE TABLE data 
-(
-id INT PRIMARY KEY,
-track_name VARCHAR(255) NOT NULL,
-);
-select * from data;
-alter table data add size_bytes INT, currency VARCHAR(255), price INT, rating_count_tot INT, rating_count_ver INT, user_rating INT, user_rating_ver INT, 
+SHOW tables;
+USE ironhack_examples;
+
+SELECT * from applestore;
+
+# 1. What are the different genres?
+SELECT distinct prime_genre FROM applestore;
+
+# 2. Which is the genre with the most apps rated?
+SELECT prime_genre, COUNT(user_rating) AS num_of_rating FROM applestore
+GROUP by prime_genre
+ORDER BY num_of_rating DESC;
+
+# 3. Which is the genre with most apps?
+SELECT COUNT(prime_genre) AS app_count, prime_genre FROM applestore
+GROUP BY prime_genre
+ORDER BY app_count DESC
+LIMIT 1;
+# the genre with most apps is 'Games'
+
+# 4. Which is the one with least?
+SELECT COUNT(prime_genre) AS app_count, prime_genre FROM applestore
+GROUP BY prime_genre
+ORDER BY app_count ASC
+LIMIT 1;
+# the genre with least apps is 'medical' apps
+
+USE ironhack_examples;
+# 5. Find the top 10 apps most rated.
+SELECT * FROM applestore
+ORDER BY rating_count_tot DESC
+LIMIT 10;
+
+SELECT * FROM applestore;
+
+# 6. Find the top 10 apps best rated by users.
+SELECT * FROM applestore
+ORDER BY user_rating DESC
+LIMIT 10;
+
+
+# 7. Take a look at the data you retrieved in question 5. Give some insights.
+# We can say that in the top 10 most rated apps, there is only one paid application.
+# In the top 3 there are 2 Social Networking apps.
+SELECT * FROM applestore
+ORDER BY rating_count_tot DESC
+LIMIT 10;
+
+
+# 8. Take a look at the data you retrieved in question 6. Give some insights.
+# There are 8 paid apps out of 10 in the top 10 best rated apps.
+SELECT * FROM applestore
+ORDER BY user_rating DESC
+LIMIT 10;
+
+
+# 9. Now compare the data from questions 5 and 6. What do you see?
+# Best rated and the apps that has most ratings are different.
+# Paid apps are better rated.
+# Free apps have more ratings
+
+
+# 10. How could you take the top 3 regarding both user ratings and number of votes?
+# We can order by two columns first by user ratings then by number of votes.
+SELECT * FROM applestore
+ORDER BY rating_count_tot DESC, user_rating DESC
+LIMIT 10;
+
+# 11. Do people care about the price of an app? Do some queries, comment why are you doing them and the results you retrieve. What is your conclusion?
+SELECT * FROM applestore
+ORDER BY rating_count_tot DESC
+LIMIT 30;
+# People tend to go for free apps
